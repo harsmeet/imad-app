9f2cc67356a23fc401fe594ab4a5d15596180c85
harsmeet <31625609+harsmeet@users.noreply.github.com>
2017-09-14 23:00:42 -1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+// move the Doremon image
+
+var img= document.getElementById('img');
+function moveRight() {
+marginLeft = marginLeft+2;
+img.style.marginLeft=marginLeft+'px';
+}
+img.onclick = function () {
+    var interval = setinterval(moveright,40);
+};
