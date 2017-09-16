aff6977ae902f5aac12bb18a0ca11e442dc95921
harsmeet <31625609+harsmeet@users.noreply.github.com>
2017-09-15 18:06:13 -1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,11 @@
 
 // Counter Code
 
-var button=document.getElementById("counter");
+var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function () {
     
-    counter=counter+1;
-    var span=document.getElementById("count");
+    counter = counter+1;
+    var span=document.getElementById('count');
     span.innerHTML=counter.toString();
 };
