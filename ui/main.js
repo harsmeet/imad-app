e9d96c5be635624471108bd1620c7b52066e5871
harsmeet <31625609+harsmeet@users.noreply.github.com>
2017-09-15 16:15:26 -1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,29 @@
 console.log('Loaded!');
 
+// change the text of the main-text div
+var element=document.getElementByID=('main-text');
+element.innertHTML='Update My Text';
+
 // move the Doremon image
 
 var img = document.getElementById('madi');
 
-var marginLeft=0;
-function moveRight() {
-marginLeft = marginLeft+5;
-img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function () {
-    var interval = setinterval(moveright,40);
-};
+
+
+
+
+
+
+
+
+
+
+
+// var marginLeft=0;
+// function moveRight() {
+// marginLeft = marginLeft+5;
+// img.style.marginLeft=marginLeft+'px';
+// }
+// img.onclick = function () {
+//     var interval = setinterval(moveright,40);
+// };
