98299936938955871497d335a09a78ce672788f0
harsmeet <31625609+harsmeet@users.noreply.github.com>
2017-09-15 18:25:30 -1200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
 app.get('/counter',function(req,res){
     counter = counter+1;
-    res.counter(counter.tostring());
+    res.counter(counter.toString());
 });
 
 app.get('/:articleName',function(req,res){
