7f0745bedc6a4c30cdad4d087861a968211ed3f3
harsmeet <31625609+harsmeet@users.noreply.github.com>
2017-09-15 18:26:04 -1200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter',function(req,res){
     counter = counter+1;
-    res.counter(counter.toString());
+    res.send(counter.toString());
 });
 
 app.get('/:articleName',function(req,res){
